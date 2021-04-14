d6b7ad17031df59106d1df3c6b3e2d15e2caffb1
Anushka Singh <anushka0105.be20@chitkara.edu.in>
2021-04-14 01:44:43 -0700

    Updates

---
+++ precache-manifest.3ec3b4b8dd3efb26958bf0b60e691427.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5533a07d5ee2b83b49de5e68625c75f2",
+    "url": "/i-Pod-Classic/index.html"
+  },
+  {
+    "revision": "232979aac52ea3ab0547",
+    "url": "/i-Pod-Classic/static/css/main.c1d9838c.chunk.css"
+  },
+  {
+    "revision": "623d2a99ddffa95676e9",
+    "url": "/i-Pod-Classic/static/js/2.4667406a.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/i-Pod-Classic/static/js/2.4667406a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "232979aac52ea3ab0547",
+    "url": "/i-Pod-Classic/static/js/main.a34829f4.chunk.js"
+  },
+  {
+    "revision": "f845196fba488b4fb59f",
+    "url": "/i-Pod-Classic/static/js/runtime-main.5efcbcdf.js"
+  },
+  {
+    "revision": "092c7d83566618c649286ba2531d1171",
+    "url": "/i-Pod-Classic/static/media/AudioTapes.092c7d83.png"
+  },
+  {
+    "revision": "a561f22a2401917d39f9dd336a3b7f57",
+    "url": "/i-Pod-Classic/static/media/Besomorph  Silent Child  IDGAF.a561f22a.png"
+  },
+  {
+    "revision": "e5374a4db2fa8fa48e3051435e17e524",
+    "url": "/i-Pod-Classic/static/media/Besomorph  Silent Child  IDGAF.e5374a4d.mp3"
+  },
+  {
+    "revision": "9365a8e759f1b3f28bec228c1726b660",
+    "url": "/i-Pod-Classic/static/media/Billie Eilish  Six Feet Under.9365a8e7.png"
+  },
+  {
+    "revision": "9cd2a7ccf2f56bea302d5fe3bb8aa6f5",
+    "url": "/i-Pod-Classic/static/media/Billie Eilish  Six Feet Under.9cd2a7cc.mp3"
+  },
+  {
+    "revision": "a4aaa9f22129f50f82fd71023e44cd40",
+    "url": "/i-Pod-Classic/static/media/BlueCubes.a4aaa9f2.png"
+  },
+  {
+    "revision": "5bb3fcdf00d62055bf72f743161dc3ea",
+    "url": "/i-Pod-Classic/static/media/BlueJelly.5bb3fcdf.png"
+  },
+  {
+    "revision": "48226638a200f30df574093f6cc4336e",
+    "url": "/i-Pod-Classic/static/media/Confetti  Ghost.48226638.png"
+  },
+  {
+    "revision": "6f01b1b3314d1c9d7b90588f1080ef8a",
+    "url": "/i-Pod-Classic/static/media/Confetti  Ghost.6f01b1b3.mp3"
+  },
+  {
+    "revision": "45dcb47f56b6e39e1fa4a26f3811628d",
+    "url": "/i-Pod-Classic/static/media/DHARIA  Sugar  Brownies.45dcb47f.mp3"
+  },
+  {
+    "revision": "56c55738f9cb8f519d7822de1fef399e",
+    "url": "/i-Pod-Classic/static/media/DHARIA  Sugar  Brownies.56c55738.png"
+  },
+  {
+    "revision": "1ef224038b615ad7b64100a177f489cc",
+    "url": "/i-Pod-Classic/static/media/Jony Love Your Voice.1ef22403.png"
+  },
+  {
+    "revision": "a2a2fa838883b86197f656b3fb422a71",
+    "url": "/i-Pod-Classic/static/media/Jony Love Your Voice.a2a2fa83.mp3"
+  },
+  {
+    "revision": "941e51f3f98607cdb5ac3135f29024f7",
+    "url": "/i-Pod-Classic/static/media/game.941e51f3.jpg"
+  },
+  {
+    "revision": "994f8bd3084b260278068de214632e59",
+    "url": "/i-Pod-Classic/static/media/settings.994f8bd3.png"
+  }
+]);
